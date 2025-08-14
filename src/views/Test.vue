9ea8d7f7ffdb598f5cb4a9a75904f37e63310cd1
MinDuck404 <duccjhin@gmail.com>
2025-08-14 22:11:07 +0700

    Refactor Test view to improve user fetching logic and add counter functionality

--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,20 +1,27 @@
+
+
+<!-- script cho auth -->
 <script setup>
-import { onMounted } from 'vue'
-import { useUserStore } from '@/stores/user'
+  import { onMounted } from 'vue'
+  import { useUserStore } from '@/stores/user'
+  import { useAuthStores } from '@/stores/auth'
+  import { storeToRefs } from 'pinia'
 
-import { useAuthStores } from '@/stores/auth'
-import { storeToRefs } from 'pinia'
+  const auth = useAuthStores()
+  auth.login({ user: { name: 'Thanh' }, token: 'abc123' })
+  const { user, token } = storeToRefs(auth)
 
-const auth = useAuthStores()
-auth.login({ user: { name: 'Thanh' }, token: 'abc123' })
-const { user, token } = storeToRefs(auth)
+  const userStore = useUserStore()
 
-const userStore = useUserStore()
+  onMounted(() => {
+    userStore.fetchUser()
+    // console.log('User in store:', auth.user)
+  })
 
-onMounted(() => {
-  userStore.fetchUser()
-  // console.log('User in store:', auth.user)
-})
+  // script cho counter
+  import { useCounter } from '@/composables/useCounter'
+  const { count, increment, decrement } = useCounter()
+
 </script>
 
 <template>
@@ -23,4 +30,10 @@ onMounted(() => {
     <p>Welcome, {{ userStore.profile?.name }}</p>
     <p>Welcome, {{ user?.name }}</p>
   </div>
+    <div>
+      <p>{{ count }}</p>
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+    </div>
 </template>
+
